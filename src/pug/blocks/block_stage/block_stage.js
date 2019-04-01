5cdb6e4dd1d676e55f5d70c4d5d36aea57b67516
Denis <toribashzmdog@mail.ru>
2019-04-01 20:20:08 +0300

    + блок stages(завершил)

--- src/pug/blocks/block_stage/block_stage.js
+++ src/pug/blocks/block_stage/block_stage.js
@@ -4,12 +4,35 @@ $(document).ready(()=>{
     let filler = '',
         block_stage = $('.block_stage'),
         i = 1;
+    block_stage.on('click',(event)=>{
+        let target = event.target;
+        if(target.tagName !== 'LI') return;
+
+        let text = $(target).text(),
+            items= $(block_stage.children().children()),
+            lineOrange = (594 / (block_stage.data('stage') - 1)) * 100 / 594;
+
+        items.each((elem)=>{
+            if(Number(elem)<Number(text)){
+                console.log(lineOrange);
+                items[elem].style.backgroundColor='#e75735';
+                items[elem].style.color='#FFF';
+                if(text !== '1')
+                    $(block_stage.children('label'))
+                        .css({'background'
+                                :'linear-gradient(to right, #e75735 '+ (lineOrange * (text - 1)) +'%, #e5e5e5 0%)'});
+            } else {
+                items[elem].style.backgroundColor='#e5e5e5';
+                items[elem].style.color='#888888'
+            }
+        });
+
+    });
 
     while(i<block_stage.data('stage')+1){
 
         filler+='<li>'+i+'</li>';
-        console.log(i);
         i++;
     }
-    block_stage.html('<ul>' + filler + '</ul>');
+    block_stage.html('<label></label>'+'<ul>' + filler + '</ul>');
 });
