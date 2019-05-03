374adca972bf023c4ba4a387f00cea33e13c4c20
Denis <toribashzmdog@mail.ru>
2019-05-03 18:16:07 +0300

    + блок calendar

--- src/pug/blocks/block_calendar/block_calendar.js
+++ src/pug/blocks/block_calendar/block_calendar.js
@@ -1,13 +1,22 @@
 
 $(document).ready(()=>{
-    $( ".calendar" ).datepicker({
+    $('.calendar').datepicker({
         inline: true,
-        firstDay: 1,
+        firstDay: 2,
         showOtherMonths: true,
         dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
         onSelect: function() {
-            let currentDate = $('.calendar').datepicker('getDate');
-            $('.ui-widget-content-day').children('p').text($.datepicker.formatDate("d", currentDate));
+            let currentDate = $(this).datepicker('getDate');
+            $('.ui-widget-content-day').children('p').text($.datepicker.formatDate('d', currentDate));
         }
     });
+    let currentDay = $('.ui-datepicker-current-day').children('a').text(),
+        currentDate = new Date();
+
+    $('.ui-widget-content-day').children('p').text(currentDay);
+
+    $('.ui-widget-content-btn').on('click', ()=>{
+        $('.calendar').datepicker('setDate', currentDate);
+        $('.ui-widget-content-day').children('p').text(currentDate.getDate());
+    })
 });
