3c6d1de0b511cb0c3b4bebf5df131e7f930eb668
Denis <toribashzmdog@mail.ru>
2019-05-14 21:16:59 +0300

    + блок video(частично)

--- src/pug/blocks/block_video/block_video.js
+++ src/pug/blocks/block_video/block_video.js
@@ -1,13 +1,19 @@
 $(document).ready(()=>{
     let controls = {
         video: $(".video").children('video'),
-        btn_play: $(".btn-play")
+        btn_play: $(".btn-play"),
+        btn_fullscreen: $(".btn-fullscreen"),
+        total: $(".total"),
+        progress: $(".current"),
+        fulscreen: false
     };
 
     let video = controls.video[0];
 
+    //Start the Video (исправить реакцию на отдельное окно)
     controls.btn_play.click(function(){
-        console.log(video.paused);
+        console.log(video.duration);
+        console.log(video.currentTime);
         if (video.paused) {
             video.play();
             $(this).attr('id', 'paused')
@@ -16,4 +22,30 @@ $(document).ready(()=>{
             $(this).attr('id', 'unpaused')
         }
     });
+
+    //Whole Process
+    video.addEventListener("timeupdate", function() {
+        let progress = Math.floor(video.currentTime) / Math.floor(video.duration);
+        controls.progress[0].style.width = Math.floor(progress * controls.total.width()) + "px";
+    }, false);
+
+    //Current Process
+    controls.total.click(function(e) {
+        let x = (e.clientX - this.offsetLeft - 600)/$(this).width();
+        video.currentTime = x * video.duration;
+    });
+
+    //Fullscreen (добавить фулскрин)
+    controls.btn_fullscreen.click(function(){
+        if(controls.fulscreen){
+            $(this).attr('id', 'off');
+            controls.fulscreen = false;
+            $(this).children('i').attr('class','fa fa-arrows-alt')
+        }else{
+            $(this).attr('id', 'on');
+            controls.fulscreen = true;
+            $(this).children('i').attr('class','fa fa-compress')
+        }
+    });
+
 });
