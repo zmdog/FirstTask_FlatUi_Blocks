93896dc955a1b65dd0978f446ac71ba8f3d096b6
Denis <toribashzmdog@mail.ru>
2019-05-15 22:19:46 +0300

    + блок video(частично)

--- src/pug/blocks/block_video/block_video.js
+++ src/pug/blocks/block_video/block_video.js
@@ -1,6 +1,7 @@
 $(document).ready(()=>{
-    let controls = {
-        video: $(".video").children('video'),
+    let block_video = $(".video-controls"),
+        controls = {
+        video: $(block_video).children('.video').children('video'),
         btn_play: $(".btn-play"),
         btn_fullscreen: $(".btn-fullscreen"),
         total: $(".total"),
@@ -10,10 +11,8 @@ $(document).ready(()=>{
 
     let video = controls.video[0];
 
-    //Start the Video (исправить реакцию на отдельное окно)
+    //Start the Video (Without the modal window)
     controls.btn_play.click(function(){
-        console.log(video.duration);
-        console.log(video.currentTime);
         if (video.paused) {
             video.play();
             $(this).attr('id', 'paused')
@@ -21,6 +20,13 @@ $(document).ready(()=>{
             video.pause();
             $(this).attr('id', 'unpaused')
         }
+    });
+
+    //Soft Controls
+    $(block_video).hover(()=>{
+        $('.video-controls').children('.controls').css({'opacity':'1'})
+    },()=>{
+        $('.video-controls').children('.controls').css({'opacity':'0'})
     });
 
     //Whole Process
@@ -31,20 +37,49 @@ $(document).ready(()=>{
 
     //Current Process
     controls.total.click(function(e) {
-        let x = (e.clientX - this.offsetLeft - 600)/$(this).width();
-        video.currentTime = x * video.duration;
+        let x = e.pageX;
+
+        if($(controls.total).css('width') === '479px'){
+            let z = (x - 650)/$(this).width();
+            video.currentTime = z * video.duration;
+        }else{
+            let z = (x - 112)/$(this).width();
+            video.currentTime = z * video.duration;
+        }
     });
 
-    //Fullscreen (добавить фулскрин)
+    //Fullscreen (починить фулскрин)
     controls.btn_fullscreen.click(function(){
         if(controls.fulscreen){
-            $(this).attr('id', 'off');
             controls.fulscreen = false;
-            $(this).children('i').attr('class','fa fa-arrows-alt')
+            $(this).attr('id', 'off');
+            $(this).children('i').attr('class','fa fa-arrows-alt');
+
+            $(block_video).css({
+                                'position':'relative',
+                                'width':'594px',
+                                'height':'335px',
+                                'top':'0',
+                                'left':'0',
+                                'transform':'translate(0, 0)'});
+
+            $(video).css({'width':'inherit', 'height':'100%'});
+            $(controls.total).css({'width':'479px'});
         }else{
-            $(this).attr('id', 'on');
             controls.fulscreen = true;
-            $(this).children('i').attr('class','fa fa-compress')
+            $(this).attr('id', 'on');
+            $(this).children('i').attr('class','fa fa-compress');
+
+            $(block_video).css({
+                                'position':'fixed',
+                                'top':'50%',
+                                'left':'50%',
+                                'transform':'translate(-50%, -50%)',
+                                'width':'90%',
+                                'height':'90%'});
+
+            $(block_video).children('.video').css({'width':'100%','height':'100%'});
+            $(controls.total).css({'width':'1001px'});
         }
     });
 
