a8283a08ddfefd2757046c606ebd9adf7815b670
Denis <toribashzmdog@mail.ru>
2019-05-21 12:44:58 +0300

    чистка и шрифты

--- src/pug/blocks/blocks_sdp-up/block_user-profile/block_user-profile.js
+++ src/pug/blocks/blocks_sdp-up/block_user-profile/block_user-profile.js
@@ -6,15 +6,16 @@ $(document).ready(function () {
             active_zone = $(elem).children('.active_zone');
 
             $(img).hover(()=>{
-
                 $(active_zone).css({'width':'100%'});
 
                 $(active_zone).hover(
                     ()=>{
-                    $(active_zone).css({'width':'100%'})},
+                        $(active_zone).attr('id', 'active');
+                        $(active_zone).css({'width':'100%'})},
 
                     ()=>{
-                    $(active_zone).css({'width':'0'})})
+                        $(active_zone).attr('id', 'inactive');
+                        $(active_zone).css({'width':'0'})})
                 },
 
                 ()=>{
