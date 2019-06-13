b61cf590906a1d4a66ae75a18433d30a5cb121bf
Denis <toribashzmdog@mail.ru>
2019-06-14 00:24:01 +0300

    Улучшил отображение значения ползунка + плавность

--- src/pug/blocks/block_slider/block_slider_default.js
+++ src/pug/blocks/block_slider/block_slider_default.js
@@ -1,30 +1,24 @@
 $(document).ready(()=>{
-    let sliderDefault = $(".slider#default"),
-        Default = $("#default");
 
-
-
-
-
-    $(sliderDefault).each(function (){
+    $(".slider#default").each(function (){
 
         $(this).html(()=>{
             let filler = '',
-                val = 0,
+                number = 0,
                 step = $(this).data("max")/4;
 
-            for(let i=0; i<5;i++){
-                filler+='<li>'+val+'</li>';
-                val+=step
+            for(let i=0; i<5; i++){
+                filler += '<li>'+number+'</li>';
+                number += step
             }
-            return '<ul>'+ filler +'</ul>';
+            return '<ul>'+filler+'</ul>';
         });
 
         $(this).slider({
             range: "min",
-            min: $(this).data("min"),
-            max: $(this).data("max"),
-            step:$(this).data("step")
+            min:  $(this).data("min"),
+            max:  $(this).data("max"),
+            step: $(this).data("step")
         });
     });
 });
--- src/pug/blocks/block_slider/block_slider_orange.js
+++ src/pug/blocks/block_slider/block_slider_orange.js
@@ -1,34 +1,18 @@
 $(document).ready(()=>{
-    let sliderOrange = $(".slider#orange");
 
+    $('.block_slider-orange').each(function(){
+        let slider = $(this).children('.slider#orange'),
+            label = $(this).find('.ui-slider-label-orange');
 
-    $(sliderOrange).each(function(){
-
-
-    $(this).slider({
-            min: $(this).data("min"),
-            max: $(this).data("max"),
-            step: $(this).data("step"),
+        $(slider).slider({
+            min: $(slider).data("min"),
+            max: $(slider).data("max"),
+            step: $(slider).data("step"),
             value: 0,
             slide: function(event, ui) {
-
-                $(this).children('span').text(' ');
-                $(this).children('label').text(ui.value)
-
+                $(label).text(ui.value);
             },
-            change: function (event,ui) {
-
-                let val = parseFloat(ui.handle.style.left) - 6.4 + '%';
-
-                $(this).children('label').css("left");
-                $(this).children('label').css('left', val);
-
-            }
         });
-
-        $(this).children('span').text(' ');
-        $(this).children('label').text('0');
-        $(this).children('label').css('left', '-6.3%');
-
+        $(label).text('0');
     });
 });
